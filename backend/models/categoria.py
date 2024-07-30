485e6c4ca42f6d3982ab152055fed780f1502e0a
RuthSantax <111710794+RuthSantax@users.noreply.github.com>
2024-07-29 23:04:09 -0300

    Sub y Categoria
    Acomodar los modelos

--- backend/models/categoria.py
+++ backend/models/categoria.py
@@ -1,11 +1,13 @@
 from conf.database import Base
 from sqlalchemy import Column, String
+from models import subcategoria as s
 from sqlalchemy.orm import relationship
 
 class Categoria(Base):
     __tablename__ = "categoria"
     id = Column(String(10), primary_key=True)
     nombre = Column(String(50))
+    subC = s.Subcategoria.subcategoria_id
     descripcion = Column(String(100), index=True)
     
     subcategorias = relationship("Subcategoria", back_populates="categoria")
